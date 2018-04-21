b01af4fa74433c3304795e6bb730f4a46d0d7863
Gabriel Faundez <gabriel.faundez@gmail.com>
2018-04-21 18:58:31 -0300

    :fire: hotfix forums creation

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,7 +4,9 @@ class PostsController < ApplicationController
   end
 
   def create
-    # comments_from_form = params['myform']['comments']
-    Category.create(name: params['post']['post_tittle'])
+    puts(params['post']['tittle'])
+    new_category = Category.create(name: params['post']['tittle'])
+    new_category.save()
+    redirect_to(forum_path())
   end
 end
