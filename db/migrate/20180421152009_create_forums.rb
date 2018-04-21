060d960d6e3aec8014d30843eb486a30349aa280
Gabriel Faundez <gabriel.faundez@gmail.com>
2018-04-21 19:50:00 -0300

    help

--- db/migrate/20180421152009_create_forums.rb
+++ db/migrate/20180421152009_create_forums.rb
@@ -1,7 +1,7 @@
 class CreateForums < ActiveRecord::Migration[5.1]
   def change
     create_table :forums do |t|
-      t.integer :id
+
       t.string :theme
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -10,31 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180421173818) do
+ActiveRecord::Schema.define(version: 20180415172448) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "categories", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "commentaries", force: :cascade do |t|
-    t.string "text"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "posts", force: :cascade do |t|
-    t.string "title"
-    t.text "content"
-    t.bigint "category_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["category_id"], name: "index_posts_on_category_id"
-  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -49,10 +28,8 @@ ActiveRecord::Schema.define(version: 20180421173818) do
     t.inet "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
 end
