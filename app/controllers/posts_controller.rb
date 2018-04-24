1277b9d13e6d55cd00cc6e67746662c9a9fb51b4
Gabriel Faundez <gabriel.faundez@gmail.com>
2018-04-24 19:15:16 -0300

    :fire: small hotfix add psot

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,7 +5,7 @@ class PostsController < ApplicationController
 
   def create
     puts(params['post']['tittle'])
-    new_category = Post.create(category: "Juegos",title: params['post']['tittle'], content: params['post']['content'])
+    new_category = Post.create(category: Category.find_by(name: 'Juegos') ,title: params['post']['tittle'], content: params['post']['content'])
     new_category.save()
     redirect_to(forum_path())
   end
