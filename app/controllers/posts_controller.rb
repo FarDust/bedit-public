45cc5add3aa711f064642dafcec311e65425d2d8
Gabriel Faundez <gabriel.faundez@gmail.com>
2018-04-26 20:50:41 -0300

    :file_folder: Merge branch 'development' of https://github.com/IIC2143-2018-1/grupo-21 into development

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,7 +9,8 @@ class PostsController < ApplicationController
 
   def create
     puts(params['post']['tittle'])
-    new_category = Post.create(category: params['post']['category'], title: params['post']['tittle'], content: params['post']['content'])
+    # new_category = Post.create(category: params['post']['category'], title: params['post']['tittle'], content: params['post']['content'])
+    new_category = Post.create(category: Category.find_by(name: 'Juegos') ,title: params['post']['tittle'], content: params['post']['content'])
     new_category.save()
     redirect_to(forum_path())
   end
