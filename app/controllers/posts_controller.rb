01307a7f260559f34174ba1e97a523d0d3dc1640
Gabriel Faundez <gabriel.faundez@gmail.com>
2018-04-26 20:39:47 -0300

    :construction: structural but still works

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,15 @@
 class PostsController < ApplicationController
-    before_action(:authenticate_user!)
+  before_action(:authenticate_user!)
+
+  def index
+  end
+
   def new
   end
 
   def create
     puts(params['post']['tittle'])
-    new_category = Post.create(category: Category.find_by(name: 'Juegos') ,title: params['post']['tittle'], content: params['post']['content'])
+    new_category = Post.create(category: params['post']['category'], title: params['post']['tittle'], content: params['post']['content'])
     new_category.save()
     redirect_to(forum_path())
   end
