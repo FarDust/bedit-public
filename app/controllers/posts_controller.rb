1e011abd50c4cf5a59ed2954ee1c7465402e7977
Fhguinez <fhguinez@uc.cl>
2018-05-07 02:09:12 -0300

    :cop: Resolve rubocop infractions

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,8 +10,8 @@ class PostsController < ApplicationController
   end
 
   def create
-    new_category = Post.create(category: Category.find_by(id: params['post']['category']), title: params['post']['tittle'], content: params['post']['content'])
-    # new_category = Post.create(category: Category.find_by(name: 'Juegos'), title: params['post']['tittle'], content: params['post']['content'])
+    new_category = Post.create(category: Category.find_by(id: params['post']['category']),
+                               title: params['post']['tittle'], content: params['post']['content'])
     new_category.save()
     redirect_to(forum_path())
   end
