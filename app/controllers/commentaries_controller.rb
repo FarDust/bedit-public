decee25d51971ccc535df326fb46802b93211acd
eavillagran <eavillagran@uc.cl>
2018-05-12 17:52:49 -0300

    :fire_engine: Add id to the commentary

--- app/controllers/commentaries_controller.rb
+++ app/controllers/commentaries_controller.rb
@@ -5,7 +5,8 @@ class CommentariesController < ApplicationController
   def create
     new_comentario = Commentary.create(
       post_id: params['commentary']['post_id'],
-      text: params['commentary']['text']
+      text: params['commentary']['text'],
+      user_id: params['commentary']['user_id']
     )
     new_comentario.save()
     redirect_to(post_path(params['commentary']['post_id']))
