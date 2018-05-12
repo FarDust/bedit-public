a1f2c1f5ac5d788d9d17d1cf90aaaf3c20d098c5
eavillagran <eavillagran@uc.cl>
2018-05-12 01:30:41 -0300

    :elephant: Try to link post with comments

--- db/migrate/20180421173818_create_commentaries.rb
+++ db/migrate/20180421173818_create_commentaries.rb
@@ -2,7 +2,7 @@ class CreateCommentaries < ActiveRecord::Migration[5.1]
   def change
     create_table(:commentaries) do |t|
       t.string(:text)
-
+      t.belongs_to(:post, index: true)
       t.timestamps()
     end
   end
