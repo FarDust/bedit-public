4dcfea5b2e05ed1a49df573666a1622692bab0b0
Gabriel Faundez <gabriel.faundez@gmail.com>
2018-05-31 18:58:52 -0400

    :file_cabinet: Configure gem and add index

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,16 @@
 class Post < ApplicationRecord
   belongs_to(:category)
   has_many(:commentary, dependent: :destroy)
+  searchable do
+    text :title, :content
+    text :commentary do
+      commentary.map(&:text)
+    end
+
+    boolean :featured
+    integer :category_id
+    # double  :average_rating
+    time    :created_at
+    time    :updated_at
+  end
 end
