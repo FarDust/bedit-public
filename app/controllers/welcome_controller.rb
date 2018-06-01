d87c5eda1570c674f1d23ee96e252a74d921a255
Gabriel Faundez <gabriel.faundez@gmail.com>
2018-06-01 16:30:44 -0400

    :star: finder by title or content ready

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,5 +3,13 @@ class WelcomeController < ApplicationController
   end
 
   def search
+    if params.key?('search')
+      title = Post.arel_table[:title]
+      content = Post.arel_table[:content]
+      @posts = Post.where(title.matches("%#{params['search']['query']}%"))
+                   .or(Post.where(content.matches("%#{params['search']['query']}%")))
+    else
+      redirect_back(fallback_location: root_path())
+    end
   end
 end
