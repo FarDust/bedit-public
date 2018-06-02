167a5883bfb48adb127d8e10533d0803ab474687
Gabriel Faundez <gabriel.faundez@gmail.com>
2018-06-01 20:59:34 -0400

    :ok_hand: replies in database

--- app/controllers/commentaries_controller.rb
+++ app/controllers/commentaries_controller.rb
@@ -1,14 +1,24 @@
 class CommentariesController < ApplicationController
+  before_action(:authenticate_user!, except: [:show])
   def index
   end
 
   def create
-    new_comentario = Commentary.create(
-      post_id: params['commentary']['post_id'],
-      text: params['commentary']['text'],
-      user_id: params['commentary']['user_id']
-    )
-    new_comentario.save()
-    redirect_to(post_path(params['commentary']['post_id']))
+    if params.key?('commentary')
+      new_comentario = Commentary.create(
+        post_id: params['commentary']['post_id'],
+        text: params['commentary']['text'],
+        user_id: params['commentary']['user_id']
+      )
+      new_comentario.save()
+      redirect_to(post_path(params['commentary']['post_id']))
+    elsif params.key?('reply')
+      new_comentario = Commentary.create(text: params['reply']['text'],
+                                         user_id: current_user.id)
+      new_comentario.commentary << Commentary.find(params['commentary_id'].to_i)
+      redirect_back(fallback_location: forum_path())
+    else
+      redirect_back(fallback_location: root_path())
+    end
   end
 end
--- app/models/commentary.rb
+++ app/models/commentary.rb
@@ -1,3 +1,5 @@
 class Commentary < ApplicationRecord
   belongs_to(:post)
+  belongs_to(:commentary)
+  has_many(:commentary, as: :replies)
 end
