686aa2bd2800fc9ecc1b8c202249303987d8fcaa
Fhguinez <fhguinez@uc.cl>
2018-06-02 17:21:26 -0400

    :warning: New models to user tipes

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,18 +10,8 @@ class User < ApplicationRecord
   has_many(:post)
   has_many(:commentary)
 
-  def roles=(roles)
-    roles = [*roles].map { |r| r.to_sym }
-    self.roles_mask = (roles & ROLES).map { |r| 2**ROLES.index(r) }.inject(0, :+)
-  end
+  has_secure_password()
 
-  def roles
-    ROLES.reject do |r|
-      ((roles_mask.to_i || 0) & 2**ROLES.index(r)).zero?
-    end
-  end
-
-  def role?(role)
-    roles.include?(role)
-  end
+  has_many(:assignments)
+  has_many(:roles, through: :assignments)
 end
