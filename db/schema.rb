82e2666543b403d173aa62f1ac61af3ec5ff46a7
Fhguinez <fhguinez@uc.cl>
2018-06-02 17:22:21 -0400

    :white_check_mark: new models testing

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180602200030) do
+ActiveRecord::Schema.define(version: 20180602210802) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "assignments", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "role_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["role_id"], name: "index_assignments_on_role_id"
+    t.index ["user_id"], name: "index_assignments_on_user_id"
+  end
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -48,6 +57,12 @@ ActiveRecord::Schema.define(version: 20180602200030) do
     t.integer "response_id"
     t.string "create"
     t.string "destroy"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "roles", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -87,4 +102,6 @@ ActiveRecord::Schema.define(version: 20180602200030) do
     t.index ["voter_id", "voter_type", "vote_scope"], name: "index_votes_on_voter_id_and_voter_type_and_vote_scope"
   end
 
+  add_foreign_key "assignments", "roles"
+  add_foreign_key "assignments", "users"
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,6 +1,8 @@
 require('test_helper')
 
 class UserTest < ActiveSupport::TestCase
+  should have_many(:assignments)
+  should have_many(:roles).through(:assignments)
   # test "the truth" do
   #   assert true
   # end
