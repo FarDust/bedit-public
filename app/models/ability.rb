02888bd2bbdea61e2f54c248ba90b5bf9bc83a4b
Gabriel Faundez <gabriel.faundez@gmail.com>
2018-06-04 10:18:19 -0400

    :ok_hand: roles working a bit

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,9 +4,10 @@ class Ability
   def initialize(user)
     user ||= User.new # guest user (not logged in)
     if user.has_role?(:admin)
-      can :manage, :all
+      can(:manage, :all)
     else
-      can :read, :all
+      can(:read, :all)
+      can(:write, Post, id: Post.with_role(:moderator, user).pluck(:id))
     end
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
-  acts_as_votable()
-  resourcify()
+  acts_as_votable() # Activate Vote
+  resourcify() # Activate Roles
   belongs_to(:category)
   has_many(:favourites)
   has_many(:commentary)
