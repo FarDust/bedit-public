f00b7a63357e7be75cae1064a518850a56b44a27
Gabriel Faundez <gabriel.faundez@gmail.com>
2018-06-13 21:00:18 -0400

    :construction: reputation defined

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,20 @@ class User < ApplicationRecord
 
   has_many(:commentaries)
   has_many(:favourites, dependent: :destroy)
-  has_many(:post)
+  has_many(:posts)
   has_many(:commentary)
+
+  def reputation()
+    total_likes = 0
+    total_dislikes = 0
+    for post in posts do
+      total_likes += post.likes.size * 1.5
+      total_dislikes += post.dislikes.size * 1.2
+    end
+    for commentary in commentaries do
+      total_likes += commentary.likes.size
+      total_dislikes += commentary.dislikes.size
+    end
+    Math.exp(total_likes) - total_dislikes*Math.log(total_dislikes)
+  end
 end
