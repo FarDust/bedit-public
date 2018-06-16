4aabf0d517529f8d2c4b78263404385a549162a2
Fhguinez <fhguinez@uc.cl>
2018-06-16 19:19:20 -0400

    :clipboard: User seed created
    The users are:
    1. admin@uc.cl
    2. moderator@uc.cl
    3. normal@uc.cl
    password for all: 123456

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+require('identicon')
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -6,11 +8,33 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+def define_role(user)
+  if user.username == 'admin'
+    user.remove_role(:normal)
+    user.add_role(:admin)
+  elsif user.username == 'moderator'
+    user.add_role(:moderator, Category.find_by(name: 'Juegos'))
+  end
+end
 
 def seed_categories
   categorias = ['Política', 'Programación', 'Juegos', 'Data Science']
   categorias.each do |name|
     Category.create(name: name)
+  end
+end
+
+def seed_users
+  users = ['normal', 'moderator', 'admin']
+  users.each do |user|
+    puts "|||||||||||||||||||||||||||||||||||||||||||#{user}||||||||||||||||||||||||||||||||||||||||||"
+    @user = User.create(
+              username: user,
+              password: '123456',
+              email: "#{user}@uc.cl",
+              avatar: Identicon.data_url_for(user)
+            )
+    define_role(@user)
   end
 end
 
@@ -40,6 +64,7 @@ def seed_commentaries
   end
 end
 
-seed_categories
-seed_posts
-seed_commentaries
+seed_categories()
+seed_users()
+seed_posts()
+seed_commentaries()
