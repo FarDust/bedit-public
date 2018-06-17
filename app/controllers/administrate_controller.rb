88e2232df5e7595b9724919afd8bb3b2e426c7f2
Fhguinez <fhguinez@uc.cl>
2018-06-17 16:34:42 -0400

    :star: Manage users table In this table you can: - See all the avatars - See all the usernames - See all the emails - Give moderation permissions to anyone - Delete any account This tables its only for admins

--- app/controllers/administrate_controller.rb
+++ app/controllers/administrate_controller.rb
@@ -30,6 +30,13 @@ class AdministrateController < ApplicationController
     redirect_to(administrate_index_path(), notice: 'Has rechazado la solicitud')
   end
 
+  def delete_user
+    @user = User.find(params[:user_id].to_i)
+    @username = @user.username
+    @user.delete()
+    redirect_to(administrate_index_path(), notice: "Has eliminado la cuenta de #{@username}")
+  end
+
   def post
   end
 end
