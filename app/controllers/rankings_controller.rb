29fd6397341f791755d2d4f35d6bbc91c60f548d
Gabriel Faundez <gabriel.faundez@gmail.com>
2018-06-18 19:04:01 -0400

    :ok_hand: users rankings}

--- app/controllers/rankings_controller.rb
+++ app/controllers/rankings_controller.rb
@@ -1,13 +1,18 @@
 class RankingsController < ApplicationController
-    
-    def users
-      @users = User.all().sort_by({ |user| [user.reputation] })[0..50]
+  def users
+    @users = User.all().sort_by do |user|
+      user.reputation
     end
+    @users = @users[0..50]
+  end
 
-    def posts
+  def posts
+    @posts = Post.all().sort_by do |post|
+      [post.get_likes.size - post.get_dislikes.size, 0].min()
     end
+    @posts = @posts[0..50]
+  end
 
-    def categories
-    end
-    
+  def categories
+  end
 end
