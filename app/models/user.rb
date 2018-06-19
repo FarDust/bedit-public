788b45ad57e919c008a4351dec4a0321d2bf0020
eavillagran <eavillagran@uc.cl>
2018-06-19 12:48:40 -0400

    :gem: Add from_omniauth method to the user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -35,4 +35,20 @@ class User < ApplicationRecord
   def assign_default_role
     add_role(:normal) if roles.blank?
   end
+
+  def self.from_omniauth(access_token)
+    data = access_token.info
+    user = User.where(email: data['email']).first
+
+    # Uncomment the section below if you want users to be created if they don't exist
+    unless user
+        user = User.create(
+          username: data['name'],
+          email: data['email'],
+          password: Devise.friendly_token[0,20]
+        )
+    end
+    user
+  end
+
 end
