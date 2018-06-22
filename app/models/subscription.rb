c61ea3a799e81232eb6f071a1a3abc215b2d8bda
Gabriel Faundez <gabriel.faundez@gmail.com>
2018-06-22 19:49:21 -0400

    :construction: model review

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,4 +1,4 @@
 class Subscription < ApplicationRecord
-  belongs_to(:user)
-  belongs_to(:categories)
+  belongs_to( :user)
+  belongs_to( :categories)
 end
--- app/models/subscrition.rb
+++
@@ -1,4 +0,0 @@
-class Subscrition < ApplicationRecord
-    belogns_to(:user, dependent: :destroy)
-    belogns_to(:category, dependent: :destroy)
-end
