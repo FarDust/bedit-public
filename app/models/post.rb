d26e9c0ae9e3b0c5ba3c791049ffa1532cd8bbad
Gabriel Faundez <gabriel.faundez@gmail.com>
2018-06-21 22:21:22 -0400

    :contruction: sort ready

--- app/models/post.rb
+++ app/models/post.rb
@@ -9,4 +9,26 @@ class Post < ApplicationRecord
   def subcribers
     favourites.length
   end
+
+  def points
+    favourites.length * [1,(get_likes.size - get_dislikes.size)].max()
+  end 
+
+  def sort_by_points
+    Post.all().sort_by do |x|
+      x.points()
+    end .reverse!
+  end
+
+  def sort_by_date
+    Post.all().sort_by do |x|
+      x.created_at
+    end .reverse!
+  end
+
+  def sort_by_trends
+    Post.all().sort_by do |x|
+      x.points * 1 / [(Date.today - x.created_at),1].min()
+    end .reverse!
+  end
 end
