0621a0f7ecc2bcab19e8329989c1ce0c88b65872
Gabriel Faundez <gabriel.faundez@gmail.com>
2018-06-22 21:54:31 -0400

    :cop: rubocop autocorrect

--- app/controllers/forums_controller.rb
+++ app/controllers/forums_controller.rb
@@ -3,13 +3,13 @@ require('will_paginate/array')
 class ForumsController < ApplicationController
   def index
     @posts = Post.all()
-    if params['order'] == 'tiempo'
-      @posts =  Post.sort_by_date(@posts)
-    elsif params['order'] == 'puntos'
-      @posts =  Post.sort_by_points(@posts)
-    else
-      @posts =  Post.sort_by_trends(@posts)
-    end
+    @posts = if params['order'] == 'tiempo'
+               Post.sort_by_date(@posts)
+             elsif params['order'] == 'puntos'
+               Post.sort_by_points(@posts)
+             else
+               Post.sort_by_trends(@posts)
+             end
     @posts = @posts.paginate(page: params[:page], per_page: 6)
     @categories = Category.all().last(4)
   end
