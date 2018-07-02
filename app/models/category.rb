163edd2e27850035919a0332eae0110f5cf98213
Gabriel Faundez <gabriel.faundez@gmail.com>
2018-07-01 21:50:31 -0400

    :file_cabinet: migrations for background

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,6 +2,7 @@ class Category < ApplicationRecord
   resourcify()
   has_many(:posts, dependent: :destroy)
   has_many(:subscriptions)
+  has_one(:background)
 
   def subcribers
     subscriptions.length
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180623011443) do
+ActiveRecord::Schema.define(version: 20180702005259) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,14 @@ ActiveRecord::Schema.define(version: 20180623011443) do
     t.boolean "answered"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "backgrounds", force: :cascade do |t|
+    t.bigint "category_id"
+    t.text "url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_backgrounds_on_category_id"
   end
 
   create_table "categories", force: :cascade do |t|
