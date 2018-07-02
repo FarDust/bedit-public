3121ef491074e7902c08100001e6cd85c97c9662
Gabriel Faundez <gabriel.faundez@gmail.com>
2018-07-02 17:03:04 -0400

    :ok_hand: loggin improvements (#74)

--- db/schema.rb
+++ db/schema.rb
@@ -23,6 +23,14 @@ ActiveRecord::Schema.define(version: 20180702051716) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "backgrounds", force: :cascade do |t|
+    t.bigint "category_id"
+    t.text "url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_backgrounds_on_category_id"
+  end
+
   create_table "categories", force: :cascade do |t|
     t.string "name"
     t.string "topic"
