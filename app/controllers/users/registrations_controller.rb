6dfcbef2ffb2d7f7b62501194ddd0d25b287b4e7
eavillagran <eavillagran@uc.cl>
2018-07-02 01:31:21 -0400

    :gem: Change method to update user profile

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -26,12 +26,34 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # PUT /resource
   def update
+    @user = User.find(current_user.id)
+
+    if current_user.provider == 'google'
+      params[:user].delete("password")
+      params[:user].delete("password_confirmation")
+      new_params = params.require(:user).permit(:email, :username)
+    else
+      new_params = params.require(:user).permit(:email, :username, :current_password, :password, :password_confirmation)  
+    end
+
     if params.key?('user') && params['user'].key?('avatar')
       avatar = params['user']['avatar']
       url = Cloudinary::Uploader.upload(avatar, options = { public_id: current_user.username })
-      params['user']['avatar'] = url['secure_url']
+      new_params['avatar'] = url['secure_url']
     end
-    super
+
+    if current_user.provider == 'google'
+      is_valid = @user.update_without_password(new_params)
+    else
+      is_valid = @user.update_with_password(new_params)
+    end
+
+    if is_valid
+      set_flash_message :notice, :updated
+      sign_in @user, :bypass => true
+      redirect_to after_update_path_for(@user)
+    end
+      
   end
 
   # DELETE /resource
